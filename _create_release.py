9a355b26b5cab5fd61aaac1a5c24fa2ac8655ae3
WojciechSobczak <wojcienty@gmail.com>
2026-01-01 14:18:06 +0100

    Utilities script enhanced and adjusted to new package structure

--- _create_release.py
+++ _create_release.py
@@ -1,34 +1,63 @@
-import os, sys
+import os
+import sys
+import argparse
+import re
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
-if "PYTHONPATH" not in os.environ: os.environ["PYTHONPATH"] = ""
-os.environ["PYTHONPATH"] = f'$PYTHONPATH{os.pathsep}{SCRIPT_DIR}'
 sys.path.append(SCRIPT_DIR)
+RELEASE_ZIP_LINK = "https://github.com/WojciechSobczak/build/archive/refs/tags/{}.zip"
 
-import build_tools
+def generate_setup_py(release_tag_name: str):
+    with open(f"{SCRIPT_DIR}/setup.py", "r", encoding="UTF-8") as file:
+        setup_py_text = file.read()
+
+    setup_py_text = setup_py_text.replace(
+        f'RELEASE_LINK = "https://github.com/WojciechSobczak/build/archive/refs/heads/master.zip"',
+        f'RELEASE_LINK = "{RELEASE_ZIP_LINK.format(release_tag_name)}"'
+    )
+    setup_py_text = setup_py_text.replace(
+        f'GITHUB_ZIP_PREFIX = "build-master/"', 
+        f'GITHUB_ZIP_PREFIX = "build-{release_tag_name}/"'
+    )
+
+    os.makedirs(f"{SCRIPT_DIR}/releases/", exist_ok=True)
+    release_file_path = f"{SCRIPT_DIR}/releases/setup.py"
+    if os.path.exists(release_file_path):
+        os.remove(release_file_path)
+    with open(release_file_path, "w", encoding="UTF-8") as file:
+        file.write(setup_py_text)
+
+def fix_readme_links(release_tag_name: str):
+    readme_file_path = f"{SCRIPT_DIR}/README.md"
+    with open(readme_file_path, "r", encoding="UTF-8") as file:
+        readme_text = file.read()
+    readme_text = re.sub(
+        r'https://github\.com/WojciechSobczak/build/releases/download/(.*)/setup\.py', 
+        f'https://github.com/WojciechSobczak/build/releases/download/{release_tag_name}/setup.py', 
+        readme_text
+    )
+    with open(readme_file_path, "w", encoding="UTF-8") as file:
+        file.write(readme_text)
 
 def main():
-    RELEASE_LINK = "https://github.com/WojciechSobczak/build/archive/{}.zip"
+    parser = argparse.ArgumentParser(description="build_tools release utils")
+    parser.add_argument('-v', '--version', default=None, type=str, help="Version string for release")
+    parser.add_argument('-g', '--generate-setup', default=True, action='store_true', help="Generate setup.py for release")
+    parser.add_argument('-r', '--readme-links-fix', default=True, help="Fix README.MD links")
+    args = parser.parse_args()
 
-    git_hash = build_tools.commons.execute_process(["git", "rev-parse", "HEAD"], SCRIPT_DIR, return_stdout=True).strip()
+    tag_name: str = args.version
+    if tag_name == None:
+        with open(f'{SCRIPT_DIR}/version.txt', "r", encoding="UTF-8") as file:
+            tag_name = file.read().strip()
+    else:
+        with open(f'{SCRIPT_DIR}/version.txt', "w", encoding="UTF-8") as file:
+            file.write(tag_name.strip())
 
-    with open(f"{SCRIPT_DIR}/setup.py", "r", encoding="UTF-8") as file:
-        text = file.read()
-        text = text.replace(
-            f'RELEASE_LINK = "https://github.com/WojciechSobczak/build/archive/refs/heads/master.zip"',
-            f'RELEASE_LINK = "{RELEASE_LINK.format(git_hash)}"'
-        )
-        text = text.replace(
-            f'GITHUB_ZIP_PREFIX = "build-master/"', 
-            f'GITHUB_ZIP_PREFIX = "build-{git_hash}/"'
-        )
-
-        os.makedirs(f"{SCRIPT_DIR}/releases/", exist_ok=True)
-        release_file_path = f"{SCRIPT_DIR}/releases/setup.py"
-        if os.path.exists(release_file_path):
-            os.remove(release_file_path)
-        with open(release_file_path, "w", encoding="UTF-8") as file:
-            file.write(text)
+    if args.generate_setup:
+        generate_setup_py(tag_name)
+    if args.readme_links_fix:
+        fix_readme_links(tag_name)
 
 if __name__ == "__main__":
     main()
--- _create_testsetup.py
+++ _create_testsetup.py
@@ -1,57 +1,77 @@
+import argparse
 import os
-import sys
 import re
 import platform
-import subprocess
 import textwrap
+from build_tools import commons
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
+TEST_PROJECT_DIR = f"{SCRIPT_DIR}/tests/{platform.system()}/"
+TEST_BUILD_TOOLS_DIR = f"{TEST_PROJECT_DIR}/.workspace/build_tools"
 
-def main():
-    TEST_PROJECT_DIR = f"{SCRIPT_DIR}/tests/{platform.system()}/"
-    TEST_BUILD_TOOLS_DIR = f"{TEST_PROJECT_DIR}/.workspace/build_tools"
-
+def generate_setups_and_copy():
     with open(f"{SCRIPT_DIR}/setup.py", "r", encoding="UTF-8") as file:
-        text = file.read()
-        regexp = re.compile(r'(os\.makedirs\(workspace_folder, exist_ok=True\)).*|\s(os\.remove\(zip_path\))')
-        matches = list(regexp.finditer(text))
+        setup_py_text = file.read()
 
-        remove_start_pos = matches[0].span()[0]
-        remove_end_pos = matches[1].span()[1]
-        text = ''.join([
-            text[0:remove_start_pos],
-            textwrap.indent(textwrap.dedent(f"""
-                def _test_copy_files():
-                    files = os.listdir("{SCRIPT_DIR}/build_tools")
-                    for file in files:
-                        if file.endswith('.py'):
-                            shutil.copy(f'{SCRIPT_DIR}/{{file}}', f'{TEST_BUILD_TOOLS_DIR}/build_tools/{{file}}')
-                _test_copy_files()
-            """), "    "),
-            text[remove_end_pos + 1:]
-        ])
+    download_text_replace_regexp = re.compile(r'(#START).*|\s(#END.*)')
+    matches = list(download_text_replace_regexp.finditer(setup_py_text))
 
-        text = text.replace('projgen_files = f"{extract_path}/build_tools/projgen_files"', f'projgen_files = f"{SCRIPT_DIR}/build_tools/projgen_files"')
+    remove_start_pos = matches[0].span()[1]
+    remove_end_pos = matches[1].span()[0]
+    setup_py_text = ''.join([
+        setup_py_text[0:remove_start_pos],
+        textwrap.indent(textwrap.dedent(f"""
+            def _test_copy_files():
+                src_projgen_dir = '{SCRIPT_DIR}/build_tools/projgen_files'
+                src_pyfiles_dir = os.path.dirname(src_projgen_dir)
+                dst_projgen_dir = '{TEST_BUILD_TOOLS_DIR}/build_tools/projgen_files'
+                dst_pyfiles_dir = os.path.dirname(dst_projgen_dir)
+                os.makedirs(dst_projgen_dir, exist_ok=True)
+
+                py_files = [f for f in os.listdir(src_pyfiles_dir) if os.path.isfile(f'{{src_pyfiles_dir}}/{{f}}')]
+                for file in py_files:
+                    shutil.copy(f'{{src_pyfiles_dir}}/{{file}}', f'{{dst_pyfiles_dir}}/{{file}}')
+                projgen_files = [f for f in os.listdir(src_projgen_dir) if os.path.isfile(f'{{src_projgen_dir}}/{{f}}')]
+                for file in projgen_files:
+                    shutil.copy(f'{{src_projgen_dir}}/{{file}}', f'{{dst_projgen_dir}}/{{file}}')
+                build_file_template_src_path = f'{{os.path.dirname(src_pyfiles_dir)}}/build_user_file.py'
+                build_file_template_dst_path = f'{{os.path.dirname(dst_pyfiles_dir)}}/build_user_file.py'
+                shutil.copy(build_file_template_src_path, build_file_template_dst_path)
+            _test_copy_files()
+        """), "    "),
+        setup_py_text[remove_end_pos + 1:]
+    ])
+    setup_py_text = setup_py_text.replace('projgen_files = f"{extract_path}/build_tools/projgen_files"', f'projgen_files = f"{SCRIPT_DIR}/build_tools/projgen_files"')
 
     os.makedirs(TEST_BUILD_TOOLS_DIR, exist_ok=True)
     with open(f"{TEST_BUILD_TOOLS_DIR}/setup.py", "w", encoding="UTF-8") as file:
-        file.write(text)
+        file.write(setup_py_text)
     with open(f"{TEST_PROJECT_DIR}/setup.py", "w", encoding="UTF-8") as file:
-        file.write(text)
+        file.write(setup_py_text)
 
-    def _run_or_die(args: list[str]):
-        process = subprocess.run(args=args, cwd = TEST_PROJECT_DIR)
-        if process.returncode != 0:
-            sys.exit(f'FAIL: args=f{args}')
+def main():
+    parser = argparse.ArgumentParser(description="build_tools test utils")
+    parser.add_argument('-ns', '--no-setup', default=False, action='store_true', help="Don't generate setup.py")
+    parser.add_argument('-f', '--fresh-test-env', default=False, action='store_true', help="Delete all test files")
+    args = parser.parse_args()
 
-    _run_or_die(["python3", "setup.py", "-g"])
-    _run_or_die(["python3", "build.py", "-d", "-m", "debug"])
-    _run_or_die(["python3", "build.py", "-d", "-m", "release"])
-    _run_or_die(["python3", "build.py", "-c", "-m", "debug"])
-    _run_or_die(["python3", "build.py", "-c", "-m", "release"])
-    _run_or_die(["python3", "build.py", "-r", "-m", "debug"])
-    _run_or_die(["python3", "build.py", "-r", "-m", "release"])
-    _run_or_die(["python3", "build.py", "--clion"])
+    if args.fresh_test_env and os.path.exists(TEST_PROJECT_DIR):
+        commons.delete_dir(TEST_PROJECT_DIR)
+    if not args.no_setup:
+        os.makedirs(TEST_PROJECT_DIR, exist_ok=True)
+        generate_setups_and_copy()
+
+    def run(args: list[str]):
+        commons.execute_process(args, cwd=TEST_PROJECT_DIR)
+
+    run(["python3", "setup.py", "-g", "-o"])
+    run(["python3", "build.py", "-d", "-m", "debug"])
+    run(["python3", "build.py", "-d", "-m", "release"])
+    run(["python3", "build.py", "-c", "-m", "debug"])
+    run(["python3", "build.py", "-c", "-m", "release"])
+    run(["python3", "build.py", "-r", "-m", "debug"])
+    run(["python3", "build.py", "-r", "-m", "release"])
+    run(["python3", "build.py", "--clion"])
 
 if __name__ == "__main__":
     main()
